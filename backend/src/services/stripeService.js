351a6435f3fe66639ea4be49b9625528f8e55296
T <tzvib8@gmail.com>
2024-02-22 22:42:41 -0500

    attempting promo in PE

--- backend/src/services/stripeService.js
+++ backend/src/services/stripeService.js
@@ -32,6 +32,8 @@ const createPaymentIntent = async (items) => {
 	const paymentIntent = await stripe.paymentIntents.create({
 		amount: total,
 		currency: "usd",
+        allow_promotion_codes: true,
+
 	})
 	return paymentIntent.client_secret
 }
