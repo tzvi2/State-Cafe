36d78bb5f5bc62200e9b4e737f137ef92c8814d3
T <tzvib8@gmail.com>
2024-05-27 14:26:17 -0400

    order dashboard now has date picker

--- frontend/src/components/dashboard/OrdersDashboard.js
+++ frontend/src/components/dashboard/OrdersDashboard.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebaseConfig';
-import { collection, orderBy, onSnapshot, query } from 'firebase/firestore';
-import styles from '../styles/dashboard/OrdersDashboard.module.css'
+import { collection, onSnapshot, query, where, orderBy } from 'firebase/firestore';
+import styles from '../styles/dashboard/OrdersDashboard.module.css';
 import { useAuth } from '../../hooks/useAuth';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function Dashboard() {
   const [orders, setOrders] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { user, signInWithGoogle } = useAuth(); 
-	const AUTHORIZED_EMAILS = ['tzvib8@gmail.com']; 
-	const isAuthorized = user && AUTHORIZED_EMAILS.includes(user.email);
+  const AUTHORIZED_EMAILS = ['tzvib8@gmail.com']; 
+  const isAuthorized = user && AUTHORIZED_EMAILS.includes(user.email);
 
   function formatTime(firestoreTimestamp) {
     if (!firestoreTimestamp) return '';
@@ -21,13 +24,20 @@ function Dashboard() {
   }
 
   useEffect(() => {
-    console.log('user change ', user)
-  }, [user])
-
+    console.log('user change ', user);
+  }, [user]);
 
   useEffect(() => {
-    // Create a query against the collection
-    const ordersQuery = query(collection(db, 'orders'), orderBy('orderedAt', 'desc'));
+    const startOfDay = new Date(selectedDate.setHours(0, 0, 0, 0));
+    const endOfDay = new Date(selectedDate.setHours(23, 59, 59, 999));
+
+    // Create a query against the collection for the selected date
+    const ordersQuery = query(
+      collection(db, 'orders'),
+      where('deliveryTime', '>=', startOfDay),
+      where('deliveryTime', '<=', endOfDay),
+      orderBy('deliveryTime', 'asc')
+    );
 
     const unsubscribe = onSnapshot(ordersQuery, (snapshot) => {
       const ordersData = snapshot.docs.map(doc => ({
@@ -41,11 +51,11 @@ function Dashboard() {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [selectedDate]);
 
   useEffect(() => {
-    console.log('orders: ', orders)
-  }, [orders])
+    console.log('orders: ', orders);
+  }, [orders]);
 
   if (user === undefined) { 
     return <p>Checking authentication...</p>;
@@ -56,9 +66,14 @@ function Dashboard() {
       <p>You need to sign in to view this page</p>
     );
   }
+
   return (
     <div className={styles.orders}>
       <h2>Orders Dashboard</h2>
+      <div className={styles.datePicker}>
+        <label>Orders Due On: </label>
+        <DatePicker selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
+      </div>
       <table>
         <thead>
           <tr>
@@ -71,48 +86,43 @@ function Dashboard() {
           </tr>
         </thead>
         <tbody>
-
-        {orders.map((order, orderIndex) => order.items.map((item, itemIndex) => {
-
-        return (
-          <tr key={`${order.id}_${itemIndex}`}>
-            {itemIndex === 0 ? (
-              <>
-                <td rowSpan={order.items.length}>{formatTime(order.orderedAt)}</td>
-                <td>
-                  {item.title}
-                  <ul>
-                    {item.options && item.options.map((option, optionIndex) => (
-                      <li key={`${order.id}_${itemIndex}_${optionIndex}`}>
-                        + {option.title}
-                      </li>
-                    ))}
-                  </ul>
-                </td>
-                <td>{item.quantity}</td>
-                <td rowSpan={order.items.length}>{order.deliveryAddress}</td>
-                <td rowSpan={order.items.length}>{formatTime(order.deliveryTime)}</td>
-                <td rowSpan={order.items.length}> {order.phoneNumber}</td>
-              </>
-            ) : (
-              <>
-                <td >
-                  {item.title}
-                  <ul>
-                    {item.options && item.options.map((option, optionIndex) => (
-                      <li key={`${order.id}_${itemIndex}_${optionIndex}`}>
-                        {option.title}
-                      </li>
-                    ))}
-                  </ul>
-                </td>
-                <td>{item.quantity}</td>
-              </>
-            )}
-          </tr>
-        );
-      }))}
-
+          {orders.map((order, orderIndex) => order.items.map((item, itemIndex) => (
+            <tr key={`${order.id}_${itemIndex}`}>
+              {itemIndex === 0 ? (
+                <>
+                  <td rowSpan={order.items.length}>{formatTime(order.orderedAt)}</td>
+                  <td>
+                    {item.title}
+                    <ul>
+                      {item.options && item.options.map((option, optionIndex) => (
+                        <li key={`${order.id}_${itemIndex}_${optionIndex}`}>
+                          + {option.title}
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                  <td>{item.quantity}</td>
+                  <td rowSpan={order.items.length}>{order.deliveryAddress}</td>
+                  <td rowSpan={order.items.length}>{formatTime(order.deliveryTime)}</td>
+                  <td rowSpan={order.items.length}>{order.phoneNumber}</td>
+                </>
+              ) : (
+                <>
+                  <td>
+                    {item.title}
+                    <ul>
+                      {item.options && item.options.map((option, optionIndex) => (
+                        <li key={`${order.id}_${itemIndex}_${optionIndex}`}>
+                          {option.title}
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                  <td>{item.quantity}</td>
+                </>
+              )}
+            </tr>
+          )))}
         </tbody>
       </table>
     </div>
