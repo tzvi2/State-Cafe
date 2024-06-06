0e70547389428cc34c32f377df55e52780544ab6
T <tzvib8@gmail.com>
2024-06-06 14:18:20 -0400

    237

--- backend/src/api/controllers/timeslotsController.js
+++ backend/src/api/controllers/timeslotsController.js
@@ -331,7 +331,6 @@ async function bookSlots(date, startTime, endTime) {
 }
 
 const handleBookTimeslot = async (req, res) => {
-  
   const { totalCookTime, date, time } = req.body; // Assuming the time is provided in a compatible format
   const deliveryBuffer = 5; // 5 minutes for delivery buffer
 
