5dc961cff3d613c429c32693932880410398034e
T <tzvib8@gmail.com>
2024-07-11 15:33:12 -0400

    new moving coffee mp4 in featured

--- frontend/src/components/home page slides/MenuSlide.js
+++ frontend/src/components/home page slides/MenuSlide.js
@@ -1,61 +1,63 @@
-// MenuSlide.jsx
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import { MenuSquare } from 'lucide-react';
 import ToggleImage from './ToggleImage';
 import styles from '../styles/home page slides styles/MenuSlide.module.css';
 
 function MenuSlide() {
-
-  const {ref} = useRef()
+  const { ref } = useRef();
   const [activeImageId, setActiveImageId] = useState(null);
 
-  const images = [
-    { 
-			id: 'breakfast', 
-			src: 'https://firebasestorage.googleapis.com/v0/b/state-cafe.appspot.com/o/featured%2Fgreen_table_70.webp?alt=media&token=3474212a-40e8-482e-93bf-02c4468d0a05', 
-			alt: 'breakfast table', 
-			overlayText: 'We offer a number of delicious breakfast options to get your day started right. Eggs, pancakes, paninis... Find something delicious now.',
-      category: "Breakfast"
-		},
-    { 
-			id: 'pasta', 
-			src: 'https://firebasestorage.googleapis.com/v0/b/state-cafe.appspot.com/o/featured%2Fbowl_of_pasta_70.webp?alt=media&token=11287db0-511b-4ade-9e46-f21770fb0f1a', 
-			alt: 'Pasta', 
-			overlayText: 'All of our pasta is homemade and fresh because you deserve the best. Guaranteed to feel and taste the difference.' ,
-      category: "Pasta"
-		},
-    { 
-			id: 'coffee', 
-			src: 'https://firebasestorage.googleapis.com/v0/b/state-cafe.appspot.com/o/featured%2Flatte_70.webp?alt=media&token=ac1a89ea-4e9c-4087-b254-a374422ee7a3', 
-			alt: 'lattes', 
-			overlayText: 'Our coffee drinks are made from whole coffee beans and are freshly ground for your drink. Try one of our homemade lattes today.',
-      category: "Coffee"
-		}
+  const items = [
+    {
+      id: 'breakfast',
+      type: 'image',
+      src: 'https://firebasestorage.googleapis.com/v0/b/state-cafe.appspot.com/o/featured%2Fgreen_table_70.webp?alt=media&token=3474212a-40e8-482e-93bf-02c4468d0a05',
+      alt: 'breakfast table',
+      overlayText: 'We offer a number of delicious breakfast options to get your day started right. Eggs, pancakes, paninis... Find something delicious now.',
+      category: 'Breakfast'
+    },
+    {
+      id: 'pasta',
+      type: 'image',
+      src: 'https://firebasestorage.googleapis.com/v0/b/state-cafe.appspot.com/o/featured%2Fbowl_of_pasta_70.webp?alt=media&token=11287db0-511b-4ade-9e46-f21770fb0f1a',
+      alt: 'Pasta',
+      overlayText: 'All of our pasta is homemade and fresh because you deserve the best. Guaranteed to feel and taste the difference.',
+      category: 'Pasta'
+    },
+    {
+      id: 'coffee',
+      type: 'video',
+      src: 'https://firebasestorage.googleapis.com/v0/b/state-cafe.appspot.com/o/featured%2Fmoving%20latte.mp4?alt=media&token=de788bcb-6385-4f9a-8fb0-66e4d0ccc355',
+      alt: 'lattes',
+      overlayText: 'Our coffee drinks are made from whole coffee beans and are freshly ground for your drink. Try one of our homemade lattes today.',
+      category: 'Coffee'
+    }
   ];
 
   const handleToggle = (id) => {
     setActiveImageId(activeImageId === id ? null : id);
   };
 
-  
   return (
     <div className={styles.slide_centered}>
-			{/* <h2>Discover our Food</h2> */}
       <div className={styles.featured_images}>
-        {images.map((image) => (
+        {items.map((item) => (
           <ToggleImage
-            key={image.id}
-            src={image.src}
-            alt={image.alt}
-            overlayText={image.overlayText}
-            onClick={() => handleToggle(image.id)}
-            isToggled={activeImageId === image.id}
-            category={image.category}
+            key={item.id}
+            type={item.type}
+            src={item.src}
+            alt={item.alt}
+            overlayText={item.overlayText}
+            onClick={() => handleToggle(item.id)}
+            isToggled={activeImageId === item.id}
+            category={item.category}
           />
         ))}
       </div>
-      <Link className={styles.button_full} to='menu'>Full Menu <MenuSquare /></Link>
+      <Link className={styles.button_full} to="menu">
+        Full Menu <MenuSquare />
+      </Link>
     </div>
   );
 }
--- frontend/src/components/home page slides/ToggleImage.js
+++ frontend/src/components/home page slides/ToggleImage.js
@@ -1,18 +1,29 @@
-import React, { useState, } from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import styles from '../styles/home page slides styles/ToggleImage.module.css';
 
-function ToggleImage({ src, alt, overlayText, category }) {
+function ToggleImage({ type, src, alt, overlayText, category }) {
   const [showOverlay, setShowOverlay] = useState(false);
+  const videoRef = useRef(null);
 
   const handleClick = () => {
     setShowOverlay(!showOverlay); 
   };
 
+  useEffect(() => {
+    if (type === 'video' && videoRef.current) {
+      videoRef.current.playbackRate = .5; // Adjust this value to control the playback speed
+    }
+  }, [type]);
+
   return (
     <div>
       <h2 className={styles.category}>{category}</h2>
       <div className={styles.imageCard} onClick={handleClick}>
-        <img loading='lazy' src={src} alt={alt}/>
+        {type === 'image' ? (
+          <img loading="lazy" src={src} alt={alt} />
+        ) : (
+          <video ref={videoRef} className={styles.video} src={src} alt={alt} autoPlay loop muted />
+        )}
         <div className={`${styles.overlay} ${showOverlay ? styles.show : styles.hide}`}>
           <span>{overlayText}</span>
         </div>
