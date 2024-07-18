e0fddd9ca72b899948e44591229fc1f2346c65f6
T <tzvib8@gmail.com>
2024-07-18 15:37:03 -0400

    cart page redesign 2.0

--- frontend/src/components/cart/CartItem.js
+++ frontend/src/components/cart/CartItem.js
@@ -1,53 +1,53 @@
-import React from 'react'
-import { useCart } from '../../hooks/useCart'
-import { Trash2 } from 'lucide-react'
-import styles from '../styles/cart styles/CartPage.module.css'
-import { centsToFormattedPrice } from '../../utils/priceUtilities'
+import React from 'react';
+import { useCart } from '../../hooks/useCart';
+import { Trash2 } from 'lucide-react';
+import styles from '../styles/cart styles/CartPage.module.css';
+import { centsToFormattedPrice } from '../../utils/priceUtilities';
 
-function CartItem({item}) {
-	const {cart, removeFromCart, updateItemQuantity} = useCart()
+function CartItem({ item }) {
+  const { removeFromCart, updateItemQuantity } = useCart();
 
-	const handleDeleteItem = () => {
-		removeFromCart(item.cartItemId)
-	}
+  const handleDeleteItem = () => {
+    removeFromCart(item.cartItemId);
+  };
 
-	const handleQuantityChange = (event) => {
-		const newQuantity = parseInt(event.target.value, 10);
-		if (!isNaN(newQuantity) && newQuantity >= 0) {
-			updateItemQuantity(item.cartItemId, newQuantity);
-		} else {
-				return;
-		}
-};
+  const handleQuantityChange = (event) => {
+    const newQuantity = parseInt(event.target.value, 10);
+    if (!isNaN(newQuantity) && newQuantity >= 0) {
+      updateItemQuantity(item.cartItemId, newQuantity);
+    }
+  };
 
-return (
-	<div className={styles.cartItem}>
-		<div className={styles.top}>
-			<select
-				className={styles.quantity}
-				value={item.quantity}
-				onChange={handleQuantityChange}
-			>
-				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
-					<option key={value} value={value}>
-						{value}
-					</option>
-				))}
-			</select>
-			<p className={styles.itemTitle}>{item.title}</p>
-			<Trash2 className={styles.delete} onClick={() => handleDeleteItem(item.cartItemId)} />
-		</div>
-		<div className={styles.bottom}>
-			<img className={styles.itemImage} src={item.img}></img>
-			<ul className={styles.options}>
-				{item.options?.map((option) => (
-					<li className={styles.option} key={`${item.cartItemId} ${option.title}`}>+ {option.title}</li>
-				))}
-			</ul>
-			<p className={styles.itemPrice}>{centsToFormattedPrice(item.total)}</p>
-		</div>
-	</div>
-);
+  return (
+    <div className={styles.cartItem}>
+			<div className={styles.col1}>
+				<img className={styles.itemImage} src={item.img} alt={item.title} />
+				<select
+          className={styles.quantity}
+          value={item.quantity}
+          onChange={handleQuantityChange}
+        >
+          {[...Array(10).keys()].map((value) => (
+            <option key={value + 1} value={value + 1}>
+              {value + 1}
+            </option>
+          ))}
+        </select>
+			</div>
+			<div className={styles.col2}>
+				<p className={styles.itemTitle}>{item.title}</p>
+				<ul className={styles.options}>
+          {item.options?.map((option) => (
+            <li className={styles.option} key={`${item.cartItemId} ${option.title}`}>+ {option.title}</li>
+          ))}
+        </ul>
+			</div>
+			<div className={styles.col3}>
+				<Trash2 className={styles.delete} onClick={handleDeleteItem} />
+				<p className={styles.itemPrice}>{centsToFormattedPrice(item.total)}</p>
+			</div>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
--- frontend/src/components/cart/CartPage.js
+++ frontend/src/components/cart/CartPage.js
@@ -1,46 +1,49 @@
-import React, { useEffect } from 'react'
-import { Trash2 } from 'lucide-react'
-import { useCart } from '../../hooks/useCart'
-import { Link } from 'react-router-dom'
-import BackArrow from '../BackArrow'
-import CartItem from './CartItem'
-import styles from '../styles/cart styles/CartPage.module.css'
+import React, { useEffect } from 'react';
+import { Trash2 } from 'lucide-react';
+import { useCart } from '../../hooks/useCart';
+import { Link } from 'react-router-dom';
+import BackArrow from '../BackArrow';
+import CartItem from './CartItem';
+import styles from '../styles/cart styles/CartPage.module.css';
 
 export default function CartPage() {
-	const {cart, removeFromCart, updateItemQuantity} = useCart()
+  const { cart, removeFromCart, updateItemQuantity } = useCart();
 
-	// useEffect(() => {
-	// 	console.log('cart items ', cart.items)
-	// }, [cart])
+  // useEffect(() => {
+  //  console.log('cart items ', cart.items)
+  // }, [cart])
 
-	return (
-		<>
-		<h2 className={styles.cartHeader}>My Cart</h2>
-		<div className={styles.cartPage}>
-		{cart.items.length > 0 ? 
-		<>
-		{cart.items.map(item => 
-				<CartItem key={item.cartItemId} item={item}/>
-		)}	
-		<p className={styles.totalRow}>
-			<span>Total: </span>
-			<span>${(cart.totalPrice / 100).toFixed(2)}</span>
-		</p>
-		<Link className={styles.btn} to="/checkout">Checkout</Link>
-
-		</> 
-		: 
-		<>
-		<p className={styles.font}>Your cart is empty 😶 </p>
-			<Link className={styles.btn} to="/menu">
-				Add something now
-			</Link>
-		
-		</>}
-		</div>
-		</>
-	)
+  return (
+    <>
+      <h2 className={styles.cartHeader}>My Cart</h2>
+      <div className={styles.cartPage}>
+        {cart.items.length > 0 ? (
+          <>
+            {cart.items.map((item) => (
+              <CartItem key={item.cartItemId} item={item} />
+            ))}
+            <p className={styles.totalRow}>
+              <span>Total: </span>
+              <span>${(cart.totalPrice / 100).toFixed(2)}</span>
+            </p>
+          </>
+        ) : (
+          <>
+            <p className={styles.font}>Your cart is empty 😶 </p>
+            <Link className={styles.btn} to="/menu">
+              Add something now
+            </Link>
+          </>
+        )}
+      </div>
+      {cart.items.length > 0 && (
+        <div className={styles.checkoutButtonWrapper}>
+          <Link className={styles.btn} to="/checkout">
+            <span>Checkout</span>
+            <span>${(cart.totalPrice / 100).toFixed(2)}</span>
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
-
-
-
