428a73c30fc8fc2cc837ba68c2d33b14fa80579c
T <tzvib8@gmail.com>
2024-07-25 22:54:16 -0400

    added caching to quick view route too

--- backend/src/api/routes/menuRoutes.js
+++ backend/src/api/routes/menuRoutes.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const { fetchMenuData, getItemByDocumentId, getItemByItemId, getItemPrice, fetchQuickView, updateMenuItemIds } = require('../controllers/menuController');
 const router = express.Router();
+const cache = require('../../../cache');
+
+const cacheMiddleware = (req, res, next) => {
+  const key = req.originalUrl;
+  const cachedResponse = cache.get(key);
+  if (cachedResponse) {
+    return res.json(cachedResponse);
+  } else {
+    res.sendResponse = res.json;
+    res.json = (body) => {
+      cache.set(key, body);
+      res.sendResponse(body);
+    };
+    next();
+  }
+};
 
 router.get('/', fetchMenuData);
-router.get('/quickView', fetchQuickView);
+router.get('/quickView', cacheMiddleware, fetchQuickView);
 router.get('/by-document-id/:documentId', getItemByDocumentId)
 router.get('/by-item-id/:itemId', getItemByItemId)
 router.get('/price/by-document-id/:documentId', getItemPrice)
