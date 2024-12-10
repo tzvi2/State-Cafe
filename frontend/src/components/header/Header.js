3e89c52d82b09f5d89081f1e3331a6992d801c9d
T <tzvib8@gmail.com>
2024-12-10 15:03:55 -0500

    header deskstop font size

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -9,7 +9,7 @@ import { useCart } from '../../hooks/useCart';
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const wrapper = useRef(null);
-  const { cart } = useCart(); 
+  const { cart } = useCart();
 
   useEffect(() => {
     const handleClickOutside = e => {
@@ -21,21 +21,17 @@ function Header() {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, [wrapper]);
 
-  // Dynamically generate class name for NavLink
-  const getNavLinkClass = (isActive, baseClass) => 
+  const getNavLinkClass = (isActive, baseClass) =>
     isActive ? `${baseClass} ${styles.activeLink}` : baseClass;
 
   return (
     <>
       <div className={styles.mobileHeader} ref={wrapper}>
         <div className={styles.topBar}>
-          <Hamburger className={styles.hamburgerReact} toggled={isOpen} toggle={() => setIsOpen(!isOpen)}/>
+          <Hamburger className={styles.hamburgerReact} toggled={isOpen} toggle={() => setIsOpen(!isOpen)} />
           <NavLink onClick={() => setIsOpen(false)} className={styles.logo} to="/">State Cafe</NavLink>
-          {/* <NavLink onClick={() => setIsOpen(false)} className={styles.logo} to="/">
-            <img src={logo}></img>
-          </NavLink> */}
           <NavLink to="cart" className={({ isActive }) => getNavLinkClass(isActive, styles.cart)}>
-            <ShoppingCart onClick={() => setIsOpen(false)} className={styles.cartIcon}/>
+            <ShoppingCart onClick={() => setIsOpen(false)} className={styles.cartIcon} />
             {cart.totalCount > 0 && <span className={styles.cartItemCount}>{cart.totalCount}</span>}
           </NavLink>
         </div>
@@ -59,10 +55,10 @@ function Header() {
         <ul className={styles.links}>
           <li><NavLink to={'/'} className={({ isActive }) => getNavLinkClass(isActive, styles.link)}>Home</NavLink></li>
           <li className={`${styles.menuLink} ${styles.isActive}`}><NavLink to={'menu'} className={({ isActive }) => getNavLinkClass(isActive, styles.link)}>Menu</NavLink></li>
-          
+
           <li><NavLink to={'about'} className={({ isActive }) => getNavLinkClass(isActive, styles.link)}>About</NavLink></li>
           <NavLink to="cart" className={({ isActive }) => getNavLinkClass(isActive, styles.cart)}>
-            <ShoppingCart onClick={() => setIsOpen(false)} className={styles.cartIcon}/>
+            <ShoppingCart onClick={() => setIsOpen(false)} className={styles.cartIcon} />
             {cart.totalCount > 0 && <span className={styles.cartItemCount}>{cart.totalCount}</span>}
           </NavLink>
         </ul>
