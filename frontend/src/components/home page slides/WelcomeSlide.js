8da4419b84928313b760ba1a02290a01bda84b58
T <tzvib8@gmail.com>
2025-01-06 12:23:33 -0500

    rm seasonal message

--- frontend/src/components/home page slides/WelcomeSlide.js
+++ frontend/src/components/home page slides/WelcomeSlide.js
@@ -8,7 +8,7 @@ function WelcomeSlide() {
 
 	return (
 		<div className={styles.welcomeSlide}>
-			<p className={styles.header}>Happy Chanukah from State Cafe.</p>
+			{/* <p className={styles.header}</p> */}
 			{/* <img src={logo} className={styles.logo}></img> */}
 			<p className={styles.sub_header}>Based in One500, we deliver <strong>ready-to-eat kosher food</strong> to the Teaneck community.</p>
 			<p className={styles.sub_header}>At State Cafe, we prioritize high quality ingredients, giving you a difference you can taste and feel.</p>
