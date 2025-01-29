1d6a921a46a067a6ef14013e2e60fc5789c5daab
T <tzvib8@gmail.com>
2025-01-28 22:35:52 -0500

    added instagram link to home page footer

--- frontend/src/components/home page slides/ContactSlide.js
+++ frontend/src/components/home page slides/ContactSlide.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Phone } from 'lucide-react'
 import styles from '../styles/home page slides styles/ContactSlide.module.css'
 import logo from '../../images/logo.png'
+import instagramIcon from '../../images/Instagram_Glyph_Gradient.png'
 
 function ContactSlide() {
 	return (
@@ -16,9 +17,15 @@ function ContactSlide() {
 					<p className={styles.text}>Email:</p>
 					<a href="mailto:support@statecafeteaneck.com">support@statecafeteaneck.com</a>
 				</div>
-				<img src={logo}></img>
+				<div className={styles.instagramLine}>
+					<p>Follow us on Instagram:</p>
+					<a className={styles.icon_container} href='https://www.instagram.com/state_cafe_teaneck' target="_blank">
+						<img className={styles.instagramIcon} src={instagramIcon} alt='Instagram'></img>
+					</a>
+				</div>
+
 			</div>
-			
+
 		</div>
 	)
 }
